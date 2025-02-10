1ff6464e9334879ca4f756c47c155ca178cc1ac1
doedcion <849172062@qq.com>
2025-02-10 12:58:31 +0800

    Create index.js
    Added pages/index.js to fix 404 error

---
+++  pages /index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to Audio Clone App!</h1>
+      <p>Your AI-powered audio cloning tool is ready 🚀</p>
+    </div>
+  );
+}
